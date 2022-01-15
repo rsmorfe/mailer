a00725c0688de3b06b40fd93ba380c85973a32f3
rsmorfe <ritchie.morfe@gmail.com>
2022-01-15 11:11:55 +0800

    code revisions on send-mail.php

--- send-email.php
+++ send-email.php
@@ -1,38 +1,38 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+    require 'vendor/autoload.php'; // If you're using Composer (recommended)
+    // Comment out the above line if not using Composer
+    // require("<PATH TO>/sendgrid-php.php");
+    // If not using Composer, uncomment the above line and
+    // download sendgrid-php.zip from the latest release here,
+    // replacing <PATH TO> with the path to the sendgrid-php.php file,
+    // which is included in the download:
+    // https://github.com/sendgrid/sendgrid-php/releases
 
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("ritchie.morfe@gmail.com", "Ritchie Morfe");
-$email->setSubject("my First Mail from SendGrid");
-$email->addTo("ritchie.morfe@gmail.com", "RitzH");
-$email->addContent("text/plain", "Congratulations! This is a sample message");
-$email->addContent(
-    "text/html", "<strong>Congratulations! This is a sample message</strong>"
-);
+    $email = new \SendGrid\Mail\Mail(); 
+    $email->setFrom("ritchie.morfe@gmail.com", "Ritchie Morfe");
+    $email->setSubject("my First Mail from SendGrid");
+    $email->addTo("ritchie.morfe@gmail.com", "RitzH");
+    $email->addContent("text/plain", "Congratulations! This is a sample message");
+    $email->addContent(
+        "text/html", "<strong>Congratulations! This is a sample message</strong>"
+    );
 
-$key = getenv('SENDGRID_API_KEY');
-$sendgrid = new \SendGrid($key);
-// $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    $key = getenv('SENDGRID_API_KEY', true) ?: getenv('SENDGRID_API_KEY');
+    $sendgrid = new \SendGrid($key);
+    // $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 
-// $apiKey = getenv('SENDGRID_API_KEY');
-// $sendgrid = new \SendGrid($apiKey);
+    echo "TESTING...\n";
+    echo(getenv('SENDGRID_API_KEY') ?: "ERROR LOADING API KEY \n");
 
 
+    try {
+        $response = $sendgrid->send($email);
+        //$response = $sendgrid->client->_("suppression/bounces")->get();
+        print $response->statusCode() . "\n";
+        print_r($response->headers());
+        print $response->body() . "\n";
+    } catch (Exception $e) {
+        echo 'Caught exception: '. $e->getMessage() ."\n";
+    }
 
-try {
-    $response = $sendgrid->send($email);
-    //$response = $sendgrid->client->_("suppression/bounces")->get();
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-    echo getenv('SENDGRID_API_KEY');
-}
+?>
